22f977df72c7ef5d86ef5a66d61969d1ec6ea639
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-27 15:58:36 +0200

    Added a simple test for the dummy interface that can later be extended to the real thing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,25 @@
 """Test Config
 """
 
+import os
 import sys
-from os import path
+import pytest
 
-sys.path.insert(1, path.abspath(path.join(path.dirname(__file__), path.pardir)))
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+@pytest.fixture(scope="session")
+def goLib():
+    libDir = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__), os.path.pardir, "pymms", "lib"
+        )
+    )
+    if not os.path.isdir(libDir):
+        return False
+
+    currDir = os.getcwd()
+    os.chdir(libDir)
+    exStatus = os.system("%s/makeLib.sh" % libDir)
+    os.chdir(currDir)
+
+    return exStatus == 0
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@
 """Core object tests.
 """
 
+import os
 import pytest
 
 from pymms import PyMMS
@@ -10,3 +11,9 @@ from pymms import PyMMS
 def testConfigInit():
     tObj = PyMMS()
     assert tObj.helloWorld()
+
+@pytest.mark.core
+def testDummyGoInterface(goLib):
+    assert goLib
+    tObj = PyMMS()
+    assert tObj.goMMS.doubleIt(21) == 42
