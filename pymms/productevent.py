56cd7e17188dc1d92ffd3c851bbb8d4b7d64793a
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-09-08 11:02:30 +0200

    Updated go-mms HEAD and added ProductEvent class

---
+++ pymms/productevent.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""py-mms product event
+
+ PY-MMS : Product Event Class
+ =============================
+
+ Copyright 2020 MET Norway
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import logging
+
+from .gomms import GoMMS
+
+logger = logging.getLogger(__name__)
+
+class ProductEvent():
+    """Wrapper class for the go-mms product event.
+    """
+
+    def __init__(self):
+
+        self.goMMS = GoMMS()
+
+        # Event properties
+        self._eventProduct = ""
+        self._eventProductSlug = ""
+        self._eventProductionHub = ""
+
+        return
+
+    ##
+    #  Getters
+    ##
+
+    @property
+    def product(self):
+        return self._eventProduct
+
+    @property
+    def productSlug(self):
+        return self._eventProductSlug
+
+    @property
+    def productionHub(self):
+        return self._eventProductionHub
+
+    ##
+    #  Setters
+    ##
+
+    @product.setter
+    def product(self, value):
+        if isinstance(value, str):
+            self._eventProduct = value
+        else:
+            self._eventProduct = ""
+            raise ValueError("ProductEvent.product must be a string")
+        return
+
+    @productSlug.setter
+    def productSlug(self, value):
+        if isinstance(value, str):
+            self._eventProductSlug = value
+        else:
+            self._eventProductSlug = ""
+            raise ValueError("ProductEvent.productSlug must be a string")
+        return
+
+    @productionHub.setter
+    def productionHub(self, value):
+        if isinstance(value, str):
+            self._eventProductionHub = value
+        else:
+            self._eventProductionHub = ""
+            raise ValueError("ProductEvent.productionHub must be a string")
+        return
+
+# END Class ProductEvent
