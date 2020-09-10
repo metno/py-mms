1ce984f0e806e1da404bfcb3bbee639303df3151
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-09-10 14:26:15 +0200

    Also build go client and server with the build script

--- makeLib.sh
+++ makeLib.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd pymms/lib/go-mms
-go build -o libmms.so -buildmode=c-shared ./export
+
+go build -v -o libmms.so -buildmode=c-shared ./export
+go build -v ./cmd/mmsd
+go build -v ./cmd/mms
+
 mv libmms.* ../
+mv mmsd ../
+mv mms ../
