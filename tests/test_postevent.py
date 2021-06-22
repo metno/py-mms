9225d70267ca26a6e14ca6c0fc84ed2e0cbd1dcd
Martin Grønlien Pejcoch <martinp@met.no>
2021-06-22 16:00:47 +0200

    Increase test coverage

--- tests/test_postevent.py
+++ tests/test_postevent.py
@@ -2,6 +2,7 @@
 """Post event object tests.
 """
 
+from typing import Counter
 import pytest
 
 from urllib import request
@@ -15,6 +16,9 @@ def testCreateProductEvent():
         productionHub="FirstC",
         productLocation="FirstD",
         eventInterval=42,
+        refTime="2021-06-22T12:00:00Z",
+        counter=1,
+        totalCount=1
     )
 
     assert pEvent.jobName == "FirstA"
@@ -22,6 +26,9 @@ def testCreateProductEvent():
     assert pEvent.productionHub == "FirstC"
     assert pEvent.productLocation == "FirstD"
     assert pEvent.eventInterval == 42
+    assert pEvent.refTime == "2021-06-22T12:00:00Z"
+    assert pEvent.counter == 1
+    assert pEvent.totalCount == 1
 
     with pytest.raises(ValueError):
         pEvent.jobName = 0
@@ -38,17 +45,32 @@ def testCreateProductEvent():
     with pytest.raises(ValueError):
         pEvent.eventInterval = "2"
 
+    with pytest.raises(ValueError):
+        pEvent.refTime = 0
+
+    with pytest.raises(ValueError):
+        pEvent.counter = "1"
+
+    with pytest.raises(ValueError):
+        pEvent.totalCount = "1"
+
     pEvent.jobName = "SecondA"
     pEvent.product = "SecondB"
     pEvent.productionHub = "SecondC"
     pEvent.productLocation = "SecondD"
     pEvent.eventInterval = 43
+    pEvent.refTime = "2000-01-01T00:00:00Z"
+    pEvent.counter = 2
+    pEvent.totalCount = 3
 
     assert pEvent.jobName == "SecondA"
     assert pEvent.product == "SecondB"
     assert pEvent.productionHub == "SecondC"
     assert pEvent.productLocation == "SecondD"
     assert pEvent.eventInterval == 43
+    assert pEvent.refTime == "2000-01-01T00:00:00Z"
+    assert pEvent.counter == 2
+    assert pEvent.totalCount == 3
 
 @pytest.mark.events
 def testSendProductEvent(monkeypatch):
